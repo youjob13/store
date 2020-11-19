05dace95dbd3ef29cdbe55726e7244108b720952
Danil Rodionow <danil.rodionow13@gmail.com>
2020-11-19 11:56:14 +0300

    layout product page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import "./App.scss";
 import Header from './component/Header/Header';
 import MainPage from "./component/MainPage/MainPage";
 import Catalog from "./component/Catalog/Catalog";
+import Product from './component/Catalog/Product/Product';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       {/* <Catalog /> */}
-      <MainPage />
+      <Product />
+      {/* <MainPage /> */}
     </div>
   );
 }
+
 export default App;
